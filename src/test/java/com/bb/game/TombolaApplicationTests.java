cd9e892a172560afb05328d8b606111316368482
Bobby <bbotev23@gmail.com>
2025-07-28 17:14:43 +0300

    Fix test build

--- src/test/java/com/bb/game/TombolaApplicationTests.java
+++ src/test/java/com/bb/game/TombolaApplicationTests.java
@@ -2,12 +2,13 @@ package com.bb.game;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TombolaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
